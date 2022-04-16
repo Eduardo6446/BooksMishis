b6752026c257501f72a83f0dd0cb575cd4bce5a6
Eduardo Antonio Orozco <74506004+Eduardo6446@users.noreply.github.com>
2022-04-16 00:23:24 -0600

    Add files via upload

---
+++ app/app.py
@@ -0,0 +1,221 @@
+
+from flask import Flask, session,render_template,request,redirect,url_for,jsonify
+from flask_session import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+import hashlib
+import os
+import requests
+
+
+app = Flask(__name__)
+
+
+# Check for environment variable
+# if not os.getenv("DATABASE_URL"):
+#     raise RuntimeError("DATABASE_URL is not set")
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Set up database
+
+
+engine = create_engine("postgresql://nqcfaquakesvau:b66550b48e768d6aa2e69054aa3806f322acd35b77e5c00ccdf9af85d634e661@ec2-44-194-92-192.compute-1.amazonaws.com:5432/d9ef2rp1qjtohv")
+db = scoped_session(sessionmaker(bind=engine))
+
+def ratingGoogleApi(isbn):
+
+    response = requests.get("https://www.googleapis.com/books/v1/volumes?q=isbn:"+isbn).json()
+
+    count = response['items'][0]['volumeInfo']['ratingsCount']
+    rating = response['items'][0]['volumeInfo']['averageRating']
+    description = response['items'][0]['volumeInfo']['description']
+    cat = response['items'][0]['volumeInfo']['categories']
+    imagen = response['items'][0]['volumeInfo']['imageLinks']['thumbnail']
+    return [count,rating,description,cat,imagen]
+
+def get_review_statistics(book_id,isbn):
+    
+    res = db.execute("SELECT count(review),round(avg(rating),2) FROM review where book_id=:id;",{'id':book_id}).fetchone()
+    
+    return [res.count,float(str(res.round))]
+
+def password_hash(password):
+    salt = "27a0091dee99016f8fb6599da096feff"
+    slat_password = password+salt
+    hashed_password = hashlib.md5(slat_password.encode())
+    return hashed_password.hexdigest()
+
+
+  
+@app.route("/")
+def index():
+    res = db.execute("select * from books LIMIT 12")
+    return render_template("index.html",res=res)
+
+
+@app.route("/search")
+def search():
+    return render_template('search.html')
+ 
+@app.route("/login",methods=['GET','POST'])
+def login():
+    isbn = request.args.get('next')
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = db.execute("select * from users where username=:username and password=:password;",{'username':username,'password':password_hash(password)})
+        if user.rowcount == 0:
+            return render_template('login.html',message="El usuario o la contraseña son incorrectos")
+        session['logged_in'] = True
+        session['username'] = request.form['username']
+        if isbn:
+            return redirect(url_for("book",isbn=isbn))
+        return redirect(url_for("search"))
+
+    return render_template('login.html',next=isbn)
+
+@app.route("/signup",methods=['GET','POST'])
+def signup():
+    isbn = request.args.get('next')
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        if password1!=password2:
+            return render_template('signup.html',message="Ingrese correctamente las contraseñas")
+           
+        try:
+            password = password_hash(password1)
+            user = db.execute("insert into users (username,password) values(:username ,:password);",{'username':username,'password':password})
+            db.commit()
+            session['logged_in'] = True
+            session['username'] = username
+            if isbn:
+                return redirect(url_for("book",isbn=isbn))
+            return redirect(url_for("search"))
+        except:
+            return render_template('signup.html',message="Este usuario ya existe")
+    return render_template('signup.html',next=isbn)
+
+
+
+@app.route("/books")
+def books():
+    q = request.args.get('search')
+    if q != None:
+        q = q.strip().replace("'","")
+        obj_books = db.execute("select * from books where isbn LIKE ('%"+q+"%')  or lower(title) LIKE lower('%"+q+"%') or  lower(author) LIKE lower('%"+q+"%') or year LIKE ('%"+q+"%') order by year desc;").fetchall() 
+        count = len(obj_books)
+        if count == 0:
+            return render_template('books.html',q=q,count=count,message="404 No encontrado")
+
+
+        
+        return render_template('books.html',q=q,count=count,obj_books=obj_books)
+    return render_template('books.html')
+
+
+@app.route("/book/<isbn>",methods=['GET','POST'])
+def book(isbn):
+    message = None
+    error = False
+    is_reviewed = False
+    if request.method == "POST" and session['logged_in']:
+        my_rating = int(request.form.get('rating'))
+        my_review = request.form.get('review')
+        book_id = request.form.get('book_id')
+        if my_review.strip()=="" or my_rating=="":
+            message = "Reseña invalida"
+        else:
+            db.execute("insert into review (username, review, rating, book_id) select :username,:review,:rating,:book_id where not exists (select * from review where username = :username and book_id = :book_id);",
+            {
+                'username': session['username'],
+                'review': my_review,
+                'rating':my_rating,
+                'book_id': book_id    
+            })
+            db.commit()
+            is_reviewed = True
+
+
+
+    res = db.execute("select * from books where isbn=:isbn;",{'isbn':isbn}).fetchone() 
+    if res==None:
+        return render_template('book.html')
+    
+
+
+    reviews = db.execute("select * from review where book_id=:id;",{'id':res.id}).fetchall()
+    if request.method == "GET":
+        try:
+            if session['logged_in']:
+                check_review = db.execute("select username from review where book_id=:id and username=:username;",
+            {
+                'id':res.id,
+                'username': session['username']  
+            }).fetchone()
+                if check_review != None:
+                    is_reviewed = True
+        except:
+            pass
+    try:
+        count,rating,description,cat,imagen = ratingGoogleApi(isbn)
+        
+    except:
+        error = True
+        count,rating = 0,0
+        description = "Sin descripción"
+        cat = "Sin categoría"
+        imagen = "https://cdn-icons-png.flaticon.com/512/580/580185.png"
+    return render_template('book.html',imagen=imagen,cat=cat,description=description,obj_book=res,count=count,rating=rating,reviews=reviews,message=message,is_reviewed=is_reviewed,error=error)
+
+
+
+@app.route("/api")
+def api():
+    return render_template('api.html')
+
+
+@app.route("/api/<isbn>")
+def api_url(isbn):
+    res = db.execute("select * from books where isbn=:isbn;",{'isbn':isbn}).fetchone()
+    if res == None:
+        return jsonify({
+            "error": "isbn Invalido.",
+            "Message": "Puede ver la documentacion en '/api' :D"
+            }),404
+    try:
+        count,rating = ratingGoogleApi(isbn)
+        
+    except:
+        count,rating = 0,0
+    
+    return jsonify({
+        "title": res.title,
+        "author": res.author,
+        "year": res.year,
+        "isbn": res.isbn,
+        "review_count": count,
+        "average_score": rating
+    }),200
+    
+
+@app.route("/logout")
+def logout():
+    
+    session.clear()
+    return redirect(url_for("login"))
+
+
+@app.errorhandler(404)
+def notFound(e):
+    return render_template("404.html"),404
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
